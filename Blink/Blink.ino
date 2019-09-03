695d783ce7acb53a12d969ad43aba967496a5351
baldalejos <baldalejos@gmail.com>
2019-09-03 18:28:08 +0200

    cambio a pin5

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,15 +1,15 @@
 
-#define pin 4
+#define pin 5
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin 
-  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(4, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(5, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
-  digitalWrite(4, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(5, LOW);    // turn the LED off by making the voltage LOW
   delay(2000);                       // wait for a second
 }
